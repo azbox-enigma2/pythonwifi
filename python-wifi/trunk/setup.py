74508dab4dcc8fda6c8139ca93f5b74c3d6ac85a
Roman Joost <roman@bromeco.de>
2008-08-01 12:59:16 +0000

    2008-08-01  Roman Joost  <roman@bromeco.de>
    	* setup.py: bumped to version 0.3.1
    
    	* version.txt
    	* PKG-INFO: removed, because they're not needed
    
    
    
    git-svn-id: file:///home/sean/tmp/python-wifi-0.3.1_plus@7 16bde9ef-9a1a-0410-9662-fd3f7015a279

--- python-wifi/trunk/setup.py
+++ python-wifi/trunk/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "python-wireless",
-    version = "0.3",
+    version = "0.3.1",
     packages = find_packages(exclude='tests'),
 
     # metadata for upload to PyPI
@@ -14,6 +14,6 @@ about a W-Lan card's capabilities, like the wireless extensions
 written in C.""",
     license = "LGPL",
     keywords = "wireless wlan iwconfig iwtools",
-    url = "http://www.romanofski.de/downloads/pywifi", 
-    download_url = "http://www.romanofski.de/downloads/pywifi", 
+    url = "https://developer.berlios.de/projects/pythonwifi/"
+    download_url = "https://developer.berlios.de/projects/pythonwifi/", 
 )
