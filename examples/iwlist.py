azbox-enigma2/pythonwifi

commit 19a06551bc395876d97ed908528878416385d595

Clean up print_channels() with report_error()
Signed-off-by: Sean Robinson <seankrobinson@gmail.com>

examples/iwlist.py
@@ -82,12 +82,7 @@ def print_channels(wifi, args=None):
            (error_number == errno.ENODEV):
             print "%-8.16s  no frequency information.\n" % (wifi.ifname, )
         else:
-            print """Uncaught error condition.  Please report this to the \
-developers' mailing list (informaion available at \
-http://lists.berlios.de/mailman/listinfo/pythonwifi-dev).  While attempting to \
-print channel informaion for %s, the error "%d - %s" occurred.""" % \
-(wifi.ifname, error_number, error_string)
-    else:
+            report_error("channel", wifi.ifname, error_number, error_string)
         print "%-8.16s  %02d channels in total; available frequencies :" % \
                     (wifi.ifname, num_frequencies)
         for channel in channels:

commit 7c63bc9b3782fdf954c734964c6ba573985d0732

BUG FIX: fix error introduced in commit 19a06551
Accidentally removed an "else".

Signed-off-by: Sean Robinson <seankrobinson@gmail.com>

examples/iwlist.py
@@ -83,6 +83,7 @@ def print_channels(wifi, args=None):
             print "%-8.16s  no frequency information.\n" % (wifi.ifname, )
         else:
             report_error("channel", wifi.ifname, error_number, error_string)
+    else:
         print "%-8.16s  %02d channels in total; available frequencies :" % \
                     (wifi.ifname, num_frequencies)
         for channel in channels: