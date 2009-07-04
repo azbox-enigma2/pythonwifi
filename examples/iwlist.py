963b3f73da563d887bfb4d0aaabb0c1218f7fefd
Sean Robinson <seankrobinson@gmail.com>
2009-07-04 13:30:23 -0700

    Fill in print_aps()
    This version may or may not match 100% since its backend is
    different from print_ap_info() in iwlist.c.
    
    Signed-off-by: Sean Robinson <seankrobinson@gmail.com>

--- examples/iwlist.py
+++ examples/iwlist.py
@@ -388,7 +388,67 @@ def print_retry(wifi, args=None):
                 print mode
 
 def print_aps(wifi, args=None):
-    pass
+    """ Print the access points detected nearby.
+
+        iwlist.c uses the deprecated SIOCGIWAPLIST, but iwlist.py uses
+        regular scanning (i.e. Wireless.scan()).
+
+    """
+    # "Check if the interface could support scanning"
+    try:
+        iwrange = Iwrange(wifi.ifname)
+    except IOError, (error_number, error_string):
+        sys.stderr.write("%-8.16s  Interface doesn't support scanning.\n\n" % (
+                            wifi.ifname))
+    else:
+        # "Check for Active Scan (scan with specific essid)"
+        # "Check for last scan result (do not trigger scan)"
+        # "Initiate Scanning"
+        try:
+            results = wifi.scan()
+        except IOError, (error_number, error_string):
+            if error_number != errno.EPERM:
+                sys.stderr.write(
+                    "%-8.16s  Interface doesn't support scanning : %s\n\n" %
+                    (wifi.ifname, error_string))
+        else:
+            if (len(results) == 0):
+                print "%-8.16s  Interface doesn't have " % (wifi.ifname, ) + \
+                      "a list of Peers/Access-Points"
+            else:
+                print "%-8.16s  Peers/Access-Points in range:"% (wifi.ifname, )
+                for ap in results:
+                    if (ap.quality.quality):
+                        if (ap.quality.updated & \
+                                    pythonwifi.flags.IW_QUAL_QUAL_UPDATED):
+                            quality_updated = "="
+                        else:
+                            quality_updated = ":"
+                        if (ap.quality.updated & \
+                                    pythonwifi.flags.IW_QUAL_LEVEL_UPDATED):
+                            signal_updated = "="
+                        else:
+                            signal_updated = ":"
+                        if (ap.quality.updated & \
+                                    pythonwifi.flags.IW_QUAL_NOISE_UPDATED):
+                            noise_updated = "="
+                        else:
+                            noise_updated = ":"
+                        print "    %s : Quality%c%s/%s  Signal level%c%s/%s  Noise level%c%s/%s" % \
+                            (ap.bssid,
+                            quality_updated,
+                            ap.quality.quality,
+                            wifi.getQualityMax().quality,
+                            signal_updated,
+                            ap.quality.getSignallevel(),
+                            "100",
+                            noise_updated,
+                            ap.quality.getNoiselevel(),
+                            "100")
+                    else:
+                        print "    %s" % (ap.bssid, )
+                print
+
 
 def report_error(function, interface, error_number, error_string):
     """ Print error to user. """
