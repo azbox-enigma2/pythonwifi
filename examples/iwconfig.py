f8374f68a72be91de2e98c6f61f932c194999ee1
Sean Robinson <seankrobinson@gmail.com>
2009-07-05 09:36:00 -0700

    Rework main() flow control to remove sys.exit() calls
    Signed-off-by: Sean Robinson <seankrobinson@gmail.com>

--- examples/iwconfig.py
+++ examples/iwconfig.py
@@ -221,28 +221,26 @@ def version_info():
 def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hv", ["help", "version"])
-    except getopt.GetoptError, err:
-        # print help information and exit:
-        print str(err) # will print something like "option -a not recognized"
-        usage()
-        sys.exit(2)
-
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            usage()
-            sys.exit(0)
-        if opt in ("-v", "--version"):
-            version_info()
-            sys.exit(0)
-
-    if len(args) == 0:
-        # no params given to iwconfig.py
-        for interface in getNICnames():
-            iwconfig(interface)
-    if len(args) == 1:
-        # one param given to iwconfig.py, it should be a network device
-        if sys.argv[1] in getNICnames():
-            iwconfig(sys.argv[1])
+    except getopt.GetoptError, error_str:
+        # invalid options will be taken to be interface name
+        pass
+    else:
+        try:
+            if opts[0][0] in ("-h", "--help"):
+                usage()
+        except:
+            try:
+                if opts[0][0] in ("-v", "--version"):
+                    version_info()
+            except:
+                if len(args) == 0:
+                    # no params given to iwconfig.py
+                    for interface in getNICnames():
+                        iwconfig(interface)
+                if len(args) == 1:
+                    # one param given to iwconfig.py, it should be a network device
+                    if sys.argv[1] in getNICnames():
+                        iwconfig(sys.argv[1])
 
 
 if __name__ == "__main__":
