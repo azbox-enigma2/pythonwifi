3ed1f2249e2a201aabed32757377c189f22cbd5c
Sean Robinson <seankrobinson@gmail.com>
2009-07-05 10:05:23 -0700

    Match iwconfig --help whitespace
    Signed-off-by: Sean Robinson <seankrobinson@gmail.com>

--- examples/iwconfig.py
+++ examples/iwconfig.py
@@ -190,26 +190,26 @@ def iwconfig(interface):
 def usage():
     """ Print info about using iwconfig.py. """
     print """Usage: iwconfig.py [interface]
-                   interface essid {NNN|any|on|off}
-                   interface mode {managed|ad-hoc|master|...}
-                   interface freq N.NNN[k|M|G]
-                   interface channel N
-                   interface bit {N[k|M|G]|auto|fixed}
-                   interface rate {N[k|M|G]|auto|fixed}
-                   interface enc {NNNN-NNNN|off}
-                   interface key {NNNN-NNNN|off}
-                   interface power {period N|timeout N|saving N|off}
-                   interface nickname NNN
-                   interface nwid {NN|on|off}
-                   interface ap {N|off|auto}
-                   interface txpower {NmW|NdBm|off|auto}
-                   interface sens N
-                   interface retry {limit N|lifetime N}
-                   interface rts {N|auto|fixed|off}
-                   interface frag {N|auto|fixed|off}
-                   interface modulation {11g|11a|CCK|OFDMg|...}
-                   interface commit
-          Check man pages for more details."""
+                interface essid {NNN|any|on|off}
+                interface mode {managed|ad-hoc|master|...}
+                interface freq N.NNN[k|M|G]
+                interface channel N
+                interface bit {N[k|M|G]|auto|fixed}
+                interface rate {N[k|M|G]|auto|fixed}
+                interface enc {NNNN-NNNN|off}
+                interface key {NNNN-NNNN|off}
+                interface power {period N|timeout N|saving N|off}
+                interface nickname NNN
+                interface nwid {NN|on|off}
+                interface ap {N|off|auto}
+                interface txpower {NmW|NdBm|off|auto}
+                interface sens N
+                interface retry {limit N|lifetime N}
+                interface rts {N|auto|fixed|off}
+                interface frag {N|auto|fixed|off}
+                interface modulation {11g|11a|CCK|OFDMg|...}
+                interface commit 
+       Check man pages for more details."""
 
 def version_info():
     """ Print version info for iwconfig.py, Wireless Extensions compatibility,
